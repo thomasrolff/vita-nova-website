9510457dae90e7897a0249d153a948d2d2b3a85a
Thomas Rolff <thomasrolff@icloud.com>
2022-09-12 00:16:26 +0200

    Set up global style and fonts

---
+++ constants/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    black: '#000000',
+    blue: '#6C7F8D',
+    offBlack: '#3C3A3A',
+    offWhite: '#FBFAF9',
+    white: '#ffffff',
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { GlobalStyle } from '../styles';
 
 function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <GlobalStyle />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 }
 
