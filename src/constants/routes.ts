d32b9f28570815486e76bacca941a3b9a0c1e729
Thomas Rolff <thomasrolff@icloud.com>
2022-10-01 13:38:03 +0200

    Add basic footer

---
+++ src/constants/routes.ts
@@ -0,0 +1,44 @@
+import { IRoutes } from '../types';
+
+export const routes: IRoutes[] = [
+    {
+        id: 0,
+        title: 'Kamers',
+        href: '/rooms',
+    },
+    {
+        id: 1,
+        title: 'Het Schip',
+        href: '/ship',
+    },
+    {
+        id: 2,
+        title: 'Scheepsruim',
+        href: '/hold',
+    },
+    {
+        id: 3,
+        title: 'Groepen',
+        href: '/groups',
+    },
+    {
+        id: 4,
+        title: 'Virtuele Tour',
+        href: 'https://my.mpskin.com/nl/tour/vitanova',
+    },
+    {
+        id: 5,
+        title: 'Praktische info',
+        href: '/info',
+    },
+    {
+        id: 6,
+        title: 'FAQs',
+        href: '/faq',
+    },
+    {
+        id: 7,
+        title: 'Contact',
+        href: '/contact',
+    },
+];
