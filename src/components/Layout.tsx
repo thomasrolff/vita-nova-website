0847705c12a020652a52d6e44841a2a18dfd686a
Thomas Rolff <thomasrolff@icloud.com>
2022-10-08 01:08:43 +0200

    Fix top margin because of fixed navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import { Footer } from './Footer';
 import { MobileMenu } from './MobileMenu';
 import { Navbar } from './Navbar';
 
 interface IProps {
+    className?: string;
     children: React.ReactNode;
 }
 
-export const Layout = ({ children }: IProps) => {
+const BaseLayout = ({ className, children }: IProps) => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <>
+        <div className={className}>
             <Navbar onHamburgerClick={() => setMenuOpen(!menuOpen)} />
             <MobileMenu open={menuOpen} />
             {children}
             <Footer />
-        </>
+        </div>
     );
 };
+
+export const Layout = styled(BaseLayout)`
+    margin-top: 56px;
+`;
