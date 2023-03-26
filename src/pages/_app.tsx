b7d2ebf9326d727c5cd0a4e0cb9c12a68aa7f3dc
Thomas Rolff <thomasrolff@icloud.com>
2023-03-26 13:34:54 +0200

    Add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { Layout } from '../components';
@@ -6,6 +7,18 @@ import { GlobalStyle } from '../styles';
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=UA-119750358-1"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-119750358-1');   
+                `}
+            </Script>
             <GlobalStyle />
             <Layout>
                 <Component {...pageProps} />
